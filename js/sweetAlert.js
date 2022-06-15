d9ef9e3a6f3ba5041b970d42a19fb4f3fbe5f264
Fabian Martinez <jorgefabianmartinez@gmail.com>
2022-06-14 21:59:02 -0300

    sweetalert

---
+++ js/sweetAlert.js
@@ -0,0 +1,15 @@
+Swal.fire({
+    title: "Hola!!",
+    text: "Vamos a ingresar los  jugadores",
+
+    confirmButtonText: 'Comenzar',
+    footer: '<span class="warning">A divertirnos!!</span>',
+    width: '90%',
+    padding: '1rem',
+    background: '#ae8d4f',
+    backdrop: true,
+    timer: 6000,
+    timerProgressBar: true,
+    imageUrl: 'https://static9.depositphotos.com/1007989/1157/i/450/depositphotos_11570416-stock-photo-playtime.jpg'
+
+});
